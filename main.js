6f028c920c9036ebcb21f7754ac998da97d826a7
Sampath Paravasthu <write2sampath@gmail.com>
2020-08-09 15:03:33 +0530

    Ready for deployment

---
+++ main.js
@@ -0,0 +1,68 @@
+const GOOGLE_TRANSLATOR = require('@danke77/google-translate-api');
+const EXPRESS = require('express');
+const bodyParser = require('body-parser');
+const app = EXPRESS();
+const CORS = require('cors');
+const PATH = require('path');
+const { env } = require('process');
+
+const DIST = PATH.join(__dirname, '/client/dist');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json())
+app.use(CORS());
+
+const DEFAULT_LANGUAGE = {
+  code: 'en',
+  name: 'english'
+};
+
+const LANGUAGES = [
+  { code: 'es', name: 'spanish' },
+  { code: 'tl', name: 'filipino', alias: 'fil' },
+  { code: 'ms', name: 'malay' },
+  { code: 'vi', name: 'vietnamese' },
+  { code: 'zh-CN', name: 'chinese_simplified', alias: 'zh' },
+  { code: 'zh-TW', name: 'chinese_traditional', alias: 'zh-Hant' }
+];
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`APP is running successfully in ${process.env.PORT || 3000}`);
+});
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(EXPRESS.static(DIST));
+  app.get('/rgstay-translator', (req, res) => {
+    res.sendFile(PATH.join(DIST, 'index.html'));
+  })
+}
+
+
+let formResponseObject = (translatedResponse) => {
+  return translatedResponse.reduce((acc, curr, indx) => {
+    LANGUAGES[indx].alias ? acc[LANGUAGES[indx].alias] = curr.text : acc[LANGUAGES[indx].code] = curr.text;
+    return acc;
+  }, {});
+};
+
+app.post('/api/translate', async (req, res) => {
+  const TEXT = req.body.textToBeTranslated;
+  const TRANSLATOR = LANGUAGES.map((language) => {
+    return new GOOGLE_TRANSLATOR({
+      from: DEFAULT_LANGUAGE.code,
+      to: language.code
+    }).translate(TEXT);
+
+  });
+  try {
+    let responses = await Promise.all(TRANSLATOR);
+    res.send({ ...{ [DEFAULT_LANGUAGE.code]: TEXT }, ...formResponseObject(responses) });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+
+
